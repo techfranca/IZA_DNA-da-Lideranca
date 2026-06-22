233cb9a8f6a71345a2ae56659853e0aff539498a
techfranca <contato@francaassessoria.com>
2026-06-22 18:47:11 -0300

    static export + otimizacao

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  output: "export",
   images: {
-    formats: ["image/webp"],
-    deviceSizes: [375, 640, 750, 828, 1080],
-    imageSizes: [200, 384, 400, 500],
+    unoptimized: true,
   },
 };
 
