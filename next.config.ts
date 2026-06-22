efe6077c35ba1156e8af0c2435223f2a5ed87cd7
techfranca <contato@francaassessoria.com>
2026-06-22 18:44:19 -0300

    otimizacao de performance

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    formats: ["image/webp"],
+    deviceSizes: [375, 640, 750, 828, 1080],
+    imageSizes: [200, 384, 400, 500],
+  },
 };
 
 export default nextConfig;
